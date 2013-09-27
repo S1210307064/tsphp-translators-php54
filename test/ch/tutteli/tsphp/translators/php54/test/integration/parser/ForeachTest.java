f0f7729b40590bc99099c1e40474f3a36edb434f
Robert Stoll <rstoll@tutteli.ch>
2013-09-27 12:54:49 +0200

    TSPHP-584 remove copyright and author block from files Cleanups

--- test/ch/tutteli/tsphp/translators/php54/test/integration/parser/ForeachTest.java
+++ test/ch/tutteli/tsphp/translators/php54/test/integration/parser/ForeachTest.java
@@ -1,38 +1,18 @@
-/*
- * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package ch.tutteli.tsphp.translators.php54.test.integration.parser;
 
 import ch.tutteli.tsphp.translators.php54.test.integration.testutils.ATranslatorParserTest;
 import ch.tutteli.tsphp.translators.php54.test.integration.testutils.ExpressionHelper;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-/**
- *
- * @author Robert Stoll <rstoll@tutteli.ch>
- */
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 @RunWith(Parameterized.class)
 public class ForeachTest extends ATranslatorParserTest
 {
@@ -42,7 +22,7 @@ public class ForeachTest extends ATranslatorParserTest
     }
 
     @Test
-    public void test() throws RecognitionException, FileNotFoundException, IOException {
+    public void test() throws RecognitionException, IOException {
         translate();
     }
 
--- test/ch/tutteli/tsphp/translators/php54/test/integration/testutils/ATest.java
+++ test/ch/tutteli/tsphp/translators/php54/test/integration/testutils/ATest.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package ch.tutteli.tsphp.translators.php54.test.integration.testutils;
 
 import ch.tutteli.tsphp.common.AstHelper;
@@ -29,10 +13,6 @@ import ch.tutteli.tsphp.parser.antlr.ErrorReportingTSPHPParser;
 import ch.tutteli.tsphp.parser.antlr.TSPHPParser;
 import ch.tutteli.tsphp.translators.php54.PrecedenceHelper;
 import ch.tutteli.tsphp.translators.php54.antlr.ErrorReportingPHP54TranslatorWalker;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.ParserRuleReturnScope;
@@ -43,10 +23,10 @@ import org.antlr.stringtemplate.StringTemplateGroup;
 import org.junit.Assert;
 import org.junit.Ignore;
 
-/**
- *
- * @author Robert Stoll <rstoll@tutteli.ch>
- */
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+
 @Ignore
 public abstract class ATest implements IErrorLogger
 {
@@ -101,7 +81,7 @@ public abstract class ATest implements IErrorLogger
     protected void typecheck() {
     }
 
-    public void translate() throws FileNotFoundException, IOException, RecognitionException {
+    public void translate() throws IOException, RecognitionException {
         parse();
 
         typecheck();
