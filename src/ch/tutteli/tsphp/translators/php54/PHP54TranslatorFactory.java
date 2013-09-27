6c327d14153b08a261041d59179a8574ff06b7e1
Robert Stoll <rstoll@tutteli.ch>
2013-09-27 12:54:49 +0200

    TSPHP-584 remove copyright and author block from files Cleanups

--- src/ch/tutteli/tsphp/translators/php54/PHP54TranslatorFactory.java
+++ src/ch/tutteli/tsphp/translators/php54/PHP54TranslatorFactory.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Robert Stoll <rstoll@tutteli.ch>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package ch.tutteli.tsphp.translators.php54;
 
 import ch.tutteli.tsphp.common.ITranslator;
@@ -23,10 +7,6 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import org.antlr.stringtemplate.StringTemplateGroup;
 
-/**
- *
- * @author Robert Stoll <rstoll@tutteli.ch>
- */
 public class PHP54TranslatorFactory implements ITranslatorFactory
 {
 
@@ -51,7 +31,7 @@ public class PHP54TranslatorFactory implements ITranslatorFactory
                     streamReader.close();
                 }
             } catch (Exception ex) {
-                //no furhter exception handling needed
+                //no further exception handling needed
             }
         }
     }
